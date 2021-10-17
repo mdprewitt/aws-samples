19065c24084f4dd544e2f93da70adbe25cc539c2
Marc Prewitt <mprewitt@gmail.com>
2021-10-17 17:03:30 -0400

    implement create custom language model

--- transcribe/create_clm.py
+++ transcribe/create_clm.py
@@ -1,17 +1,40 @@
-import boto3
+import argparse
+import logging
+from pathlib import PurePosixPath as Path
 
-def main():
+import boto3
+from botocore.exceptions import ClientError
+
+
+def main(clm_file, bucket, role_arn):
+    s3_client = boto3.client('s3')
+    model_name = "TestModel"
+    model_prefix = model_name
+    try:
+        s3_client.upload_file(clm_file, bucket, f"{model_prefix}/{Path(clm_file).name}")
+    except ClientError as e:
+        logging.error(e)
+
     transcribe_client = boto3.client('transcribe')
+    s3_uri = "s3://" + str(Path(bucket) / model_prefix)
+    logging.info("uploading to %s", s3_uri)
     response = transcribe_client.create_language_model(
         LanguageCode='en-US',
         BaseModelName='WideBand',
-        ModelName='TestModel',
+        ModelName=model_name,
         InputDataConfig={
-            'S3Uri': 'string',
-            'TuningDataS3Uri': 'string',
-            'DataAccessRoleArn': 'string'
+            'S3Uri': s3_uri,
+            # 'TuningDataS3Uri': 'string',
+            'DataAccessRoleArn': role_arn
         },
     )
 
+
 if __name__ == '__main__':
-    main()
+    logging.basicConfig(level=logging.DEBUG)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("file")
+    parser.add_argument("bucket")
+    parser.add_argument("role")
+    args = parser.parse_args()
+    main(args.file, args.bucket, args.role)
